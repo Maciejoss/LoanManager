54a281f6f9c819ee8f2958449c9b764c6c89aaa4
Jakub Borek <01158592@pw.edu.pl>
2023-01-19 21:48:25 +0100

    After merge fixes

--- LoansAnalyzer/src/app/main-page/side-menu/side-menu.component.ts
+++ LoansAnalyzer/src/app/main-page/side-menu/side-menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, NgZone, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserInfo } from 'src/app/Models/UserInfo/UserInfo';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-side-menu',
@@ -11,7 +10,6 @@ import { AuthService } from 'src/app/services/auth.service';
 export class SideMenuComponent {
 
   constructor(private router: Router,
-    private service: AuthService,
     private _ngZone: NgZone) {}
 
     @Input() userInfo:UserInfo|null = null;
@@ -20,16 +18,4 @@ export class SideMenuComponent {
     CloseMenu(){
         this.close.emit(null);
     }
-
-      public logout(){
-        this.service.signOutExternal();
-        this.userInfo = null;
-      }
-
-      public login(){
-        this.service.signOutExternal();
-        this._ngZone.run(() => {
-          this.router.navigate(['login-page']).then(() => window.location.reload());
-        })
-      }
 }
